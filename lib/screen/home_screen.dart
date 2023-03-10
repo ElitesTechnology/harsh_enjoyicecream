45700bfe8c3e144409521ac4fd6d74430d98c2a9
RomanBhimani <106570965+RomanBhimani@users.noreply.github.com>
2023-03-10 12:17:51 +0530

    update2

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -63,7 +63,7 @@ class _Home_ScreenState extends State<Home_Screen> {
               ),
             ),
             ListTile(
-              title: Text('Item 1'),
+              title: Text('Item 1asdsddsdcdscdsdsdsvsdcds'),
               onTap: () {
                 // TODO: implement item 1 action
               },
