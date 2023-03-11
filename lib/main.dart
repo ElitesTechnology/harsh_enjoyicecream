092dd2522e00ac1a790b418c7e30b858cd779283
RomanBhimani <106570965+RomanBhimani@users.noreply.github.com>
2023-03-11 13:06:39 +0530

    update2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   await Firebase.initializeApp();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Spalsh_Screen(),
+    home: DynamicGridView(),
   ));
 }
 
@@ -26,3 +26,43 @@ void main() async {
 //     ),
 //   );
 // }
+
+class DynamicGridView extends StatefulWidget {
+  @override
+  _DynamicGridViewState createState() => _DynamicGridViewState();
+}
+
+class _DynamicGridViewState extends State<DynamicGridView> {
+  List<String> _items = List.generate(30, (index) => "Item $index");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dynamic GridView"),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: _items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              print("Item $index clicked.");
+            },
+            child: Container(
+              color: Colors.grey[200],
+              child: Center(
+                child: Text(_items[index]),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
