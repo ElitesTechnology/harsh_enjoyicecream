d8fcd80529f8fda5e740aaab8905fabd361bb059
RomanBhimani <106570965+RomanBhimani@users.noreply.github.com>
2023-03-21 13:21:08 +0530

    update3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:core';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:harsh_enjoyicecream/screen/home_screen.dart';
-import 'package:harsh_enjoyicecream/screen/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
